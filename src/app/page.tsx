dbc90dc5c347e6bad4d1c8b640a02f58bfd1d71e
Henry Sun <Henry37@users.noreply.github.com>
2024-11-03 17:14:01 +0800

    [init] Create yml for deploying
    [init] Disable the optimization of next image
    
    [init] Update CI/CD
    
    [init] Update CI/CD
    
    [feature] Remove useless tag
    
    [init] Update CI/CD
    
    [init] Update message
    
    [init] Update CI/CD
    
    [init] Update CI/CD
    
    [init] list the contents of out folder
    
    [init] Try to add export
    
    [init] fix export issue
    
    [init] try to fix cp issue
    
    [init] Try to fix the issue of deploying
    
    [init] Fix the issue of deployment
    
    [init] fix deploy issue
    
    [init] Remove redundant import core
    
    [init] Update the permission
    
    [init] update node version
    
    [init] Fix deploy issue
    
    [init] add permission of id-token
    
    [init] Fix artifacts
    
    [init] Fix the path for artifacts
    
    [init] Fix upload artifacts
    
    [init] add path back
    
    [init] decide out to be uploaded
    
    [init] add download back
    
    [init] use new version
    
    [init] add list to debug
    
    [init] download path
    
    [init] update yml
    
    [init] Update yml
    
    [init] update version
    
    [init] update yml
    
    [init] update yml
    
    [init] update yml
    
    [init] update yml
    
    [init] update yml
    
    [init] update yml
    
    [init] update yml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,6 @@ export default function Home() {
 
         <div className={styles.ctas}>
           <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Deploy now
-          </a>
-          <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
