02cfe6c4ed2f90505e1eceb3102e42f544974f90
Henry Sun <henry.sun@student.com>
2025-11-22 12:42:35 +0800

    [feature] Redirect to a new server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
 'use client'
 
-import { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import Title from '../components/Title/Title';
-import Navigator from '../components/Navigator/Navigator';
-import BookShelf from '../components/BookShelf/BookShelf';
 import { Page } from './page.styles';
-import { books } from '../constants/books';
+import { Spin } from 'antd';
 
 const Home: FC = () => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = 'https://3mk112lv0534.vicp.fun';
+    }, 1200);
+  }, []);
+
   return (
-    <Page>
-      <Title>Welcome to visit Henry</Title>
-      <Navigator />
-      <BookShelf books={books} />
+    <Page style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '80vh' }}>
+      <div style={{ marginBottom: 24, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Title>Welcome to visit Henry, redirecting </Title>
+        <Spin size="large" />
+      </div>
     </Page>
   );
 };
